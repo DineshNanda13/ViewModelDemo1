1eb2586fdebf9c69af910348b330687656bef925
Dinesh Nanda <dannynanda76@gmail.com>
2020-09-05 10:01:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/viewmodeldemo1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.viewmodeldemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodeldemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        //binding.countText.text = count.toString()
+        binding.countText.text = viewModel.getCurrentCount().toString()
+
+        binding.button.setOnClickListener {
+            //count++
+            binding.countText.text = viewModel.getUpdatedCount().toString()
+        }
+    }
+}
